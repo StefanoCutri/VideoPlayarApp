f517218303b0918b6553c32df9c4c8475f3cf189
Stefano Cutri <stefanocutristecco@gmail.com>
2023-03-29 22:59:45 -0300

    Video replay implemented

--- app/src/main/java/com/example/videoplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/videoplayerapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.videoplayerapp
 
+import android.media.session.MediaController
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.VideoView
+import com.example.videoplayerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initialize()
     }
+       private fun initialize(){
+          with(binding){
+              videoView.setVideoPath("android.resource://"
+                      +packageName+"/"
+                      +R.raw.sunrise
+              )
+              val mediaController = android.widget.MediaController(this@MainActivity)
+              mediaController.setAnchorView(videoView)
+              mediaController.setMediaPlayer(videoView)
+              videoView.setMediaController(mediaController)
+              videoView.start()
+          }
+
+        }
+
+
+
+
 }
