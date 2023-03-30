d272a7c4241167a965500385fb90f229555b581d
Stefano Cutri <stefanocutristecco@gmail.com>
2023-03-30 17:55:36 -0300

    Add video form internet functionality

--- app/src/main/java/com/example/videoplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/videoplayerapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.videoplayerapp
 
 import android.media.session.MediaController
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.VideoView
@@ -21,11 +22,26 @@ class MainActivity : AppCompatActivity() {
                       +packageName+"/"
                       +R.raw.sunrise
               )
+
+              //Media controller 1
               val mediaController = android.widget.MediaController(this@MainActivity)
               mediaController.setAnchorView(videoView)
               mediaController.setMediaPlayer(videoView)
               videoView.setMediaController(mediaController)
+              //Start video
               videoView.start()
+
+              //Video 2 uri
+              val uri = Uri.parse("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")
+              videoView2.setVideoURI(uri)
+
+              //Media controller 2
+              val mediaController2 = android.widget.MediaController(this@MainActivity)
+              mediaController2.setAnchorView(videoView2)
+              mediaController2.setMediaPlayer(videoView2)
+              videoView2.setMediaController(mediaController2)
+              //Start video
+              videoView2.start()
           }
 
         }
